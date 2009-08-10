462f713ec7c3b5dec71391a5e7e50b04b75ae5d7
jakearchibald <jake.archibald@bbc.co.uk>
2009-08-10 15:18:45 +0100

    Tidyup of manual tests (mainly deleting)

--- manualtests/forms/check_username.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$username = strToLower($_REQUEST['username']);
-	if (empty($username)) { print "ERROR"; }
-	else if ($username == "glow") { print "NOT AVAILABLE"; }
-	else { print "AVAILABLE"; }
-?>
